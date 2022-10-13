012f624e8a16a42c748e0fc8cfccb02cc5cb729f
Indy Adira Khalfani <indyadira@gmail.com>
2022-10-13 22:12:26 +0700

    mencoba ipk

--- hello world.cpp
+++ hello world.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int main()
 {
     string nama,npm,gender; //string  = tipe data, nama/npm/gender = variabel; 
+    float ipk;
     cout<<"input nama = "; //printf("nama = %s ",&nama);
     cin>>nama;
     cout<<"input npm  = ";
     cin>>npm;
     cout<<"input gender = ";
     cin>>gender;
+    cout<<"ipk          = ";
+    cin>>ipk;
 
     cout<<"nama = " <<nama <<endl;
     cout<<"npm  = " <<npm <<endl;
+    cout<<"ipk  = " <<ipk <<endl;
 }
