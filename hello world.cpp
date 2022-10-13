256fe27d517dbdd0d03c986b436d00d71cb83194
Indy Adira Khalfani <indyadira@gmail.com>
2022-10-13 22:10:09 +0700

    pengenalan

---
+++ hello world.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string nama,npm,gender; //string  = tipe data, nama/npm/gender = variabel; 
+    cout<<"input nama = "; //printf("nama = %s ",&nama);
+    cin>>nama;
+    cout<<"input npm  = ";
+    cin>>npm;
+    cout<<"input gender = ";
+    cin>>gender;
+
+    cout<<"nama = " <<nama <<endl;
+    cout<<"npm  = " <<npm <<endl;
+}
